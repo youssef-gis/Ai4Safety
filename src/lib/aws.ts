e4d0be044d45d09852c68bb9d5b1b2d25c806f2d
YOUSSEF <40327134+youssef-gis@users.noreply.github.com>
2026-01-16 20:04:19 +0100

    Add non-null assertion for AWS credentials

--- src/lib/aws.ts
+++ src/lib/aws.ts
@@ -3,8 +3,8 @@ import {S3Client}  from "@aws-sdk/client-s3";
 const s3 = new S3Client({
     region: process.env.AWS_REGION,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
     },
 });
 
